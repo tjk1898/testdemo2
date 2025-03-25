866cfe62fbfbbc1faa1688476b955f193af2c347
Tian.jinkun <tjk1898@gmail.com>
2025-03-25 16:51:14 +0800

    初始化项目

---
+++ src/test/java/edu/czjtu/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.czjtu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
